feda6da480d960b686d2dbd5b5bddc1ad2b43e4b
Thejas Devadiga <96642748+ThejasDevadiga@users.noreply.github.com>
2022-09-24 11:17:00 +0530

    .

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='obesity',
+      version='1.0',
+      description='Python',
+      author='Thejas Devadiga',
+      author_email='mrdevaidgatj@gmail.com,
+      url='.........',
+      packages=['matplotlib', 'numpy','pandas'],
+     )
